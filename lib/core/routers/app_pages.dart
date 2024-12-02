14b3fd61bd24a05fd2b79b95e7fb20c84f2285c8
MORVAN Loïs <lmorvan6@myges.fr>
2024-12-02 15:49:39 +0100

    feat: add routing for onboarding, login, and signup pages

---
+++ lib/core/routers/app_pages.dart
@@ -0,0 +1,28 @@
+import 'package:fintrack/onboarding/onboarding_page.dart';
+import 'package:get/get.dart';
+import 'package:fintrack/auth/login_page.dart';
+import 'package:fintrack/auth/signup_page.dart';
+
+class AppRoutes {
+  static const String onboarding = '/onboarding';
+  static const String login = '/login';
+  static const String signup = '/signup';
+  static const String home = '/home';
+}
+
+class AppPages {
+  static final pages = [
+    GetPage(
+      name: AppRoutes.onboarding,
+      page: () => const OnboardingPage(),
+    ),
+    GetPage(
+      name: AppRoutes.login,
+      page: () => const LoginPage(),
+    ),
+    GetPage(
+      name: AppRoutes.signup,
+      page: () => const SignupPage(),
+    ),
+  ];
+}

51eb6de921ed0d0cd8f2cd2772bded5f849d4ce9
MORVAN Loïs <lmorvan6@myges.fr>
2024-12-03 12:15:26 +0100

    feat: replace dotenv with envied for environment variable management and update Firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:fintrack/core/constants/app_colors.dart';
 import 'package:fintrack/core/main/controllers/navigation_controller.dart';
 import 'package:fintrack/core/routers/app_pages.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() async {
-  await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,

1066a97826dd0f9825333582a7b0fb9129efaad5
MORVAN Loïs <lmorvan6@myges.fr>
2024-12-03 08:41:01 +0100

    feat: initialize Firebase in main application entry point

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:fintrack/core/constants/app_colors.dart';
 import 'package:fintrack/core/routers/app_pages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 

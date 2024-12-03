65f91c4ecf304b08aa06c730a33aac852eac2969
MORVAN Loïs <lmorvan6@myges.fr>
2024-12-03 08:40:55 +0100

    feat: configure Firebase integration and update dependencies for Android

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,10 @@ import 'package:fintrack/core/constants/app_colors.dart';
 import 'package:fintrack/core/routers/app_pages.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load();
   runApp(const MyApp());
 }
 

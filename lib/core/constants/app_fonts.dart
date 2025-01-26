096be80cc080ed52d4b1e890fb42e7d72b9e454b
MORVAN Loïs <lmorvan6@myges.fr>
2025-01-26 16:55:18 +0100

    feat: update text styles to include primary text color from AppColors

--- lib/core/constants/app_fonts.dart
+++ lib/core/constants/app_fonts.dart
@@ -1,3 +1,4 @@
+import 'package:fintrack/core/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
 class AppFonts {
@@ -7,28 +8,33 @@ class AppFonts {
     fontFamily: primaryFont,
     fontSize: 24.0,
     fontWeight: FontWeight.bold,
+    color: AppColors.textPrimary,
   );
 
   static const TextStyle h2 = TextStyle(
     fontFamily: primaryFont,
     fontSize: 20.0,
     fontWeight: FontWeight.bold,
+    color: AppColors.textPrimary,
   );
 
   static const TextStyle body = TextStyle(
     fontFamily: primaryFont,
     fontSize: 16.0,
+    color: AppColors.textPrimary,
   );
 
   static const TextStyle button = TextStyle(
     fontFamily: primaryFont,
     fontSize: 24.0,
     fontWeight: FontWeight.bold,
+    color: AppColors.textPrimary,
   );
 
   static const TextStyle smallButton = TextStyle(
     fontFamily: primaryFont,
     fontSize: 16.0,
     fontWeight: FontWeight.bold,
+    color: AppColors.textPrimary,
   );
 }
